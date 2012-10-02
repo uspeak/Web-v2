3bf9f90bca08027bef2b2693eb9ea4d0b6d94013
Syrus Akbary <me@syrusakbary.com>
2012-10-02 14:47:39 +0200

    Updated jQuery CDN version

--- app/js/libs/jquery/jquery.js
+++ app/js/libs/jquery/jquery.js
@@ -1,5 +1,5 @@
 if( !window.jQuery ) {
-	define(['Console', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js'], function (Console) {
+	define(['Console', 'https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js'], function (Console) {
 		Console.group("Entering jQuery module.");
 		Console.info("jQuery: ", $);
 		Console.groupEnd();
